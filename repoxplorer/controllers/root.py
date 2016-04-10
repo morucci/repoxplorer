3612a3d6969a820bf4e8ce1478bc6fc351fd38dd
Fabien Boucher <fabien.dot.boucher@gmail.com>
2016-04-10 22:00:22 +0200

    Add project page and initial histo display

--- repoxplorer/controllers/root.py
+++ repoxplorer/controllers/root.py
@@ -1,5 +1,7 @@
 from pecan import expose
 
+from repoxplorer import index
+from repoxplorer.index.commits import Commits
 from repoxplorer.index.projects import Projects
 
 
@@ -9,3 +11,16 @@ class RootController(object):
     def index(self):
         projects = Projects().get_projects()
         return {'projects': projects}
+
+    @expose(template='project.html')
+    def project(self, pid=None):
+        c = Commits(index.Connector())
+        projects = Projects().get_projects()
+        project = projects[pid]
+        p_filter = []
+        for p in project:
+            p_filter.append("%s:%s:%s" % (p['uri'],
+                p['name'], p['branch']))
+        histo = c.get_commits_histo(projects=p_filter)
+        return {'pid': pid,
+                'histo': histo}
