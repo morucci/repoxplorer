bc4cce6d6c6943a57ad35e3e49865633c133d769
Fabien Boucher <fabien.dot.boucher@gmail.com>
2016-08-11 23:14:26 +0200

    Initial code of the trends module
    Change-Id: Iafa5ef7226ab993542cd13105967fa37affa6f15

---
+++ repoxplorer/tests/tests_trends.py
@@ -0,0 +1,88 @@
+from unittest import TestCase
+
+from repoxplorer import index
+from repoxplorer.index.commits import Commits
+from repoxplorer.trends.commits import CommitsAmountTrend
+
+
+class TestCommitsAmountTrend(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.con = index.Connector(index='repoxplorertest')
+        cls.c = Commits(cls.con)
+        cls.t = CommitsAmountTrend(cls.con)
+        cls.commits = [
+            {
+                'sha': '3597334f2cb10772950c97ddf2f6cc17b184',
+                'author_date': 1410456005,
+                'committer_date': 1410456010,
+                'ttl': 5,
+                'author_name': 'Nakata Daisuke',
+                'committer_name': 'Nakata Daisuke',
+                'author_email': 'n.suke@joker.org',
+                'committer_email': 'n.suke@joker.org',
+                'projects': [
+                    'https://github.com/nakata/monkey.git:monkey:master', ],
+                'line_modifieds': 10,
+                'merge_commit': False,
+                'commit_msg': 'Add init method',
+            },
+            {
+                'sha': '3597334f2cb10772950c97ddf2f6cc17b185',
+                'author_date': 1410457005,
+                'committer_date': 1410457005,
+                'ttl': 0,
+                'author_name': 'Keiko Amura',
+                'committer_name': 'Keiko Amura',
+                'author_email': 'keiko.a@joker.org',
+                'committer_email': 'keiko.a@joker.org',
+                'projects': [
+                    'https://github.com/nakata/monkey.git:monkey:master', ],
+                'line_modifieds': 100,
+                'merge_commit': False,
+                'commit_msg': 'Merge "Fix sanity unittest"',
+            },
+            {
+                'sha': '3597334f2cb10772950c97ddf2f6cc17b186',
+                'author_date': 1410458005,
+                'committer_date': 1410458005,
+                'ttl': 0,
+                'author_name': 'Jean Bon',
+                'committer_name': 'Jean Bon',
+                'author_email': 'jean.bon@joker.org',
+                'committer_email': 'jean.bon@joker.org',
+                'projects': [
+                    'https://github.com/nakata/monkey.git:monkey:master', ],
+                'line_modifieds': 200,
+                'merge_commit': False,
+                'commit_msg': 'Add request customer feature 19',
+            },
+        ]
+        cls.c.add_commits(cls.commits)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.con.ic.delete(index=cls.con.index)
+
+    def test_get_trend(self):
+        ret = self.t.get_trend(
+            projects=['https://github.com/nakata/monkey.git:monkey:master'],
+            period_a=(1410457000, 1410459000),
+            period_b=(1410456005, 1410456015))
+        self.assertEqual(ret[0], 1)
+        self.assertEqual(ret[1], 50)
+
+        ret = self.t.get_trend(
+            projects=['https://github.com/nakata/monkey.git:monkey:master'],
+            period_a=(1410457000, 1410459000),
+            period_b=(1410455005, 1410455015))
+        self.assertEqual(ret[0], 2)
+        self.assertEqual(ret[1], 100)
+
+        ret = self.t.get_trend(
+            projects=['https://github.com/nakata/monkey.git:monkey:master'],
+            period_a=(1410455005, 1410455015),
+            period_b=(1410457000, 1410459000))
+        self.assertEqual(ret[0], -2)
+        self.assertEqual(ret[1], -100)
---
+++ repoxplorer/trends/commits.py
@@ -0,0 +1,44 @@
+# Copyright 2016, Fabien Boucher
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
+import logging
+
+from repoxplorer.index.commits import Commits
+
+logger = logging.getLogger(__name__)
+
+
+class CommitsAmountTrend(object):
+    def __init__(self, connector=None):
+        self.ic = Commits(connector)
+
+    def get_trend(self, mails=[], projects=[],
+                  period_a=None, period_b=None,
+                  merge_commit=None):
+        """ Return the amount diff and the percentil
+        of amount evolution for perdiod a compared to
+        period b
+        """
+        assert isinstance(period_a, tuple)
+        assert isinstance(period_b, tuple)
+        c_amnt_a = self.ic.get_commits_amount(mails, projects,
+                                              period_a[0], period_a[1],
+                                              merge_commit)
+        c_amnt_b = self.ic.get_commits_amount(mails, projects,
+                                              period_b[0], period_b[1],
+                                              merge_commit)
+        diff = c_amnt_a - c_amnt_b
+        trend = diff * 100 / (c_amnt_a or c_amnt_b)
+        return diff, trend
