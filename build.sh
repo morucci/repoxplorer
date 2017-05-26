736e48e63acad03ecf6ffce48f286a40fdcc08bb
Fabien Boucher <fboucher@redhat.com>
2017-05-26 13:47:03 +0000

    Update build.sh to take a version as argument
    Change-Id: Ifb43308262974430ca2226dfae7063fd5fa6872a

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+version=$1
+
 spectool -gf -S repoxplorer.spec
 mock -r /etc/mock/epel-7-x86_64.cfg --buildsrpm --sources . --spec repoxplorer.spec 
-mock -r /etc/mock/epel-7-x86_64.cfg --copyout /builddir/build/SRPMS/repoxplorer-0.8.0-1.el7.centos.src.rpm .
-mock -r /etc/mock/epel-7-x86_64.cfg -i --rebuild repoxplorer-0.8.0-1.el7.centos.src.rpm 
-mock -r /etc/mock/epel-7-x86_64.cfg --copyout /builddir/build/RPMS/repoxplorer-0.8.0-1.el7.centos.noarch.rpm .
+mock -r /etc/mock/epel-7-x86_64.cfg --copyout /builddir/build/SRPMS/repoxplorer-${version}.el7.centos.src.rpm .
+mock -r /etc/mock/epel-7-x86_64.cfg -i --rebuild repoxplorer-${version}.el7.centos.src.rpm 
+mock -r /etc/mock/epel-7-x86_64.cfg --copyout /builddir/build/RPMS/repoxplorer-${version}.el7.centos.noarch.rpm .
