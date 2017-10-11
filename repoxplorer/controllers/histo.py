048cd227e5db907a14558974811b52a7a5988e71
Fabien Boucher <fboucher@redhat.com>
2017-10-11 15:54:03 +0200

    Make the histo endpoint use resolv_filter as well
    TG-461
    
    Change-Id: If1cb947468f6d477dfc6e4fa3f4c0c986f01cafa

--- repoxplorer/controllers/histo.py
+++ repoxplorer/controllers/histo.py
@@ -13,10 +13,7 @@
 #  limitations under the License.
 
 from pecan import conf
-from pecan import abort
 from pecan import expose
-
-from datetime import datetime
 
 from repoxplorer import index
 from repoxplorer.controllers import utils
@@ -30,107 +27,18 @@ xorkey = conf.get('xorkey') or 'default'
 
 class HistoController(object):
 
-    def build_query(self, pid, tid, cid, gid, dfrom, dto,
-                    inc_merge_commit, inc_repos,
-                    metadata, exc_groups, idents):
-
-        if inc_merge_commit != 'on':
-            include_merge_commit = False
-        else:
-            # The None value will return all whatever
-            # the commit is a merge one or not
-            include_merge_commit = None
-
-        if dfrom:
-            dfrom = datetime.strptime(dfrom, "%Y-%m-%d").strftime('%s')
-        if dto:
-            dto = datetime.strptime(dto, "%Y-%m-%d").strftime('%s')
-        _metadata = []
-
-        if metadata:
-            metadata_splitted = metadata.split(',')
-            for meta in metadata_splitted:
-                try:
-                    key, value = meta.split(':')
-                    if value == '*':
-                        value = None
-                except ValueError:
-                    continue
-                _metadata.append((key, value))
-        metadata = _metadata
-
-        mails_to_exclude = {}
-
-        mails_neg = False
-        if exc_groups:
-            mails_neg = True
-            groups_splitted = exc_groups.split(',')
-            for gid in groups_splitted:
-                _, group = idents.get_group_by_id(gid)
-                mails_to_exclude.update(group['emails'])
-
-        projects_index = Projects()
-        repos = {}
-
-        query_kwargs = {}
-
-        if pid:
-            repos = projects_index.get_projects().get(pid)
-            if not repos:
-                abort(404,
-                      detail="The project has not been found")
-            query_kwargs.update(
-                {'mails': mails_to_exclude,
-                 'mails_neg': mails_neg})
-        elif tid:
-            repos = projects_index.get_tags().get(tid)
-            if not repos:
-                abort(404,
-                      detail="The tag has not been found")
-            query_kwargs.update(
-                {'mails': mails_to_exclude,
-                 'mails_neg': mails_neg})
-
-        if gid:
-            gid, group = idents.get_group_by_id(gid)
-            if not group:
-                abort(404,
-                      detail="The group has not been found")
-            mails = group['emails']
-            domains = group.get('domains', [])
-            query_kwargs.update({'mails': mails})
-            query_kwargs.update({'domains': domains})
-        elif cid:
-            cid = utils.decrypt(xorkey, cid)
-            iid, ident = idents.get_ident_by_id(cid)
-            if not ident:
-                # No ident has been declared for that contributor
-                iid, ident = idents.get_ident_by_email(cid)
-            mails = ident['emails']
-            query_kwargs.update({'mails': mails})
-
-        p_filter = utils.get_references_filter(repos, inc_repos)
-
-        query_kwargs.update({
-            'repos': p_filter,
-            'fromdate': dfrom,
-            'todate': dto,
-            'merge_commit': include_merge_commit,
-            'metadata': metadata,
-        })
-
-        return query_kwargs
-
     @expose('json')
     def authors(self, pid=None, tid=None, cid=None, gid=None,
                 dfrom=None, dto=None, inc_merge_commit=None,
                 inc_repos=None, metadata="", exc_groups=None):
 
+        projects_index = Projects()
         idents = Contributors()
 
-        query_kwargs = self.build_query(
-            pid, tid, cid, gid, dfrom, dto, inc_merge_commit, inc_repos,
-            metadata, exc_groups, idents)
+        query_kwargs = utils.resolv_filters(
+            projects_index, idents, pid, tid, cid, gid,
+            dfrom, dto, inc_repos, inc_merge_commit,
+            metadata, exc_groups)
 
         c = Commits(index.Connector(index=indexname))
         if not c.get_commits_amount(**query_kwargs):
@@ -155,11 +63,13 @@ class HistoController(object):
                 dfrom=None, dto=None, inc_merge_commit=None,
                 inc_repos=None, metadata="", exc_groups=None):
 
+        projects_index = Projects()
         idents = Contributors()
 
-        query_kwargs = self.build_query(
-            pid, tid, cid, gid, dfrom, dto, inc_merge_commit, inc_repos,
-            metadata, exc_groups, idents)
+        query_kwargs = utils.resolv_filters(
+            projects_index, idents, pid, tid, cid, gid,
+            dfrom, dto, inc_repos, inc_merge_commit,
+            metadata, exc_groups)
 
         c = Commits(index.Connector(index=indexname))
         if not c.get_commits_amount(**query_kwargs):
