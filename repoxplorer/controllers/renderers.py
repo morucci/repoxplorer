e1691251a0e5a7e152c8476ef58fe01263d0cb6d
Fabien Boucher <fboucher@redhat.com>
2017-11-30 11:44:11 +0100

    Make the CSV renderer able to render data list
    For instance if data is:
    
    [{'f1': ['a', 'b'], 'f2': 'abc'}] the render will be able
    to render ['a', 'b'] such as:
    
    f1,f2
    a;b,abc
    
    Then the comma is stricly the separator and semi column must
    not be interpreted as a separator. CSV readers may need
    to be configured when loading data.
    
    TG-722
    
    Change-Id: I015e33b97b9edae82d31cc8b65483265143b7460

--- repoxplorer/controllers/renderers.py
+++ repoxplorer/controllers/renderers.py
@@ -1,3 +1,18 @@
+# Copyright 2017, Fabien Boucher
+# Copyright 2017, Red Hat
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import csv
 from StringIO import StringIO
 
@@ -14,15 +29,26 @@ class CSVRenderer(object):
         for e in namespace:
             assert isinstance(e, dict)
         keys = namespace[0].keys()
+        keys.sort()
         w = csv.DictWriter(buf, fieldnames=keys)
         w.writeheader()
         for e in namespace:
             d = {}
             for k, v in e.items():
+                if not any([
+                        isinstance(v, basestring),
+                        isinstance(v, list),
+                        isinstance(v, int),
+                        isinstance(v, float)]):
+                    raise ValueError(
+                        "'%s' (type: %s) is not supported for CSV output" % (
+                            str(v), type(v)))
                 if isinstance(v, unicode):
                     d[k] = v.encode('utf-8', 'ignore')
+                elif isinstance(v, list):
+                    d[k] = ";".join(v)
                 else:
-                    d[k] = v
+                    d[k] = str(v)
             w.writerow(d)
         buf.seek(0)
         return buf.read()
---
+++ repoxplorer/tests/test_renderer.py
@@ -0,0 +1,48 @@
+# Copyright 2017, Fabien Boucher
+# Copyright 2017, Red Hat
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
+from unittest import TestCase
+
+from repoxplorer.controllers import renderers
+
+
+class TestCSVRenderer(TestCase):
+
+    def setUp(self):
+        self.csvrender = renderers.CSVRenderer(None, None)
+
+    def test_rendering(self):
+        data = {'f1': 'd1', 'f2': 2}
+        output = self.csvrender.render(None, data)
+        self.assertTrue(
+            output == "f1,f2\r\nd1,2\r\n")
+
+        data = {'f1': ['e1', 'e2'], 'f2': 2}
+        output = self.csvrender.render(None, data)
+        self.assertTrue(
+            output == "f1,f2\r\ne1;e2,2\r\n")
+
+        data = [
+            {'f1': 'd1', 'f2': 2},
+            {'f1': 'd2', 'f2': 3},
+        ]
+
+        output = self.csvrender.render(None, data)
+        self.assertTrue(
+            output == "f1,f2\r\nd1,2\r\nd2,3\r\n")
+
+        data = {'f1': {}}
+        self.assertRaises(ValueError, self.csvrender.render, None, data)
