933682965cdf0de92db0b786b85c5ff5c3da8dc4
Fabien Boucher <fboucher@redhat.com>
2019-01-04 11:40:22 +0100

    Projects definition stored in elasticsearch
    The current implementation have limitations in terms of
    performances and scaling. Big deployement can experiences
    high memory usage issue in the indexer and rest process.
    
    This patch dumps the projects definition in the ElasticSearch
    instance. Flat files are read then dumped in the ELS.
    
    This add an option to deactivate the dump of the definition
    in EL. The struct build (prior to the dump) can take
    some time and one can want to trigger it externaly
    
    TODO:
    - perform manual tests
    
    Change-Id: Ief98c8d2a14e49fba3c94a2136ee4891d74a2a15

--- repoxplorer/index/projects.py
+++ repoxplorer/index/projects.py
@@ -13,15 +13,18 @@
 #  limitations under the License.
 
 
-import os
 import copy
 import logging
-import cPickle
 
 from datetime import datetime
 
+from elasticsearch.helpers import bulk
+from elasticsearch.helpers import BulkIndexError
+from elasticsearch.helpers import scan as scanner
+
 from pecan import conf
 
+from repoxplorer import index
 from repoxplorer.index import YAMLDefinition
 from repoxplorer.index import date2epoch
 
@@ -29,7 +32,7 @@ from repoxplorer.index import date2epoch
 logger = logging.getLogger(__name__)
 
 
-project_templates_schema = """
+project_templates_schema = r"""
 $schema: http://json-schema.org/draft-04/schema
 
 definitions:
@@ -87,7 +90,6 @@ properties:
             type: boolean
 """
 
-
 project_templates_example = """
 templates:
   default:
@@ -114,7 +116,7 @@ templates:
     index-tags: true
 """
 
-projects_schema = """
+projects_schema = r"""
 $schema: http://json-schema.org/draft-04/schema
 
 type: object
@@ -188,26 +190,200 @@ projects:
 """
 
 
+class EProjects(object):
+
+    PROPERTIES = {
+        "aname": {"type": "string"},
+        "name": {"type": "string", "index": "not_analyzed"},
+        "description": {"type": "string"},
+        "logo": {"type": "binary"},
+        "meta-ref": {"type": "boolean", "index": "not_analyzed"},
+        "bots-group": {"type": "string", "index": "not_analyzed"},
+        "index-tags": {"type": "boolean", "index": "not_analyzed"},
+        "refs": {
+            "type": "nested",
+            "properties": {
+                "aname": {"type": "string"},
+                "name": {"type": "string", "index": "not_analyzed"},
+                "uri": {"type": "string", "index": "not_analyzed"},
+                "gitweb": {"type": "string", "index": "not_analyzed"},
+                "branch": {"type": "string", "index": "not_analyzed"},
+                "tags": {"type": "string", "index": "not_analyzed"},
+                "fullrid": {"type": "string", "index": "not_analyzed"},
+                "shortrid": {"type": "string", "index": "not_analyzed"},
+                "paths": {"type": "string", "index": "not_analyzed"},
+                "parsers": {"type": "string", "index": "not_analyzed"},
+                "index-tags": {"type": "boolean", "index": "not_analyzed"},
+                "releases": {
+                    "type": "nested",
+                    "properties": {
+                        "name": {"type": "string", "index": "not_analyzed"},
+                        "date": {"type": "string", "index": "not_analyzed"},
+                        }
+                    }
+                }
+            }
+        }
+
+    def __init__(self, connector=None):
+        self.es = connector.es
+        self.ic = connector.ic
+        self.index = connector.index
+        self.dbname = 'projects'
+        self.mapping = {
+            self.dbname: {
+                "properties": self.PROPERTIES,
+            }
+        }
+        if not self.ic.exists_type(index=self.index,
+                                   doc_type=self.dbname):
+            self.ic.put_mapping(index=self.index, doc_type=self.dbname,
+                                body=self.mapping)
+
+    def create(self, projects):
+        def gen(docs):
+            for pid, doc in docs:
+                _doc = copy.deepcopy(doc)
+                _doc['name'] = pid
+                _doc['aname'] = pid
+                _doc['refs'] = doc['repos']
+                del _doc['repos']
+                d = {}
+                d['_index'] = self.index
+                d['_type'] = self.dbname
+                d['_op_type'] = 'create'
+                d['_id'] = pid
+                d['_source'] = _doc
+                yield d
+        try:
+            bulk(self.es, gen(projects))
+        except BulkIndexError as exc:
+            errs = [e['create']['error'] for e in exc[1]]
+            if not all([True for e in errs if
+                        e['type'] == 'document_already_exists_exception']):
+                raise Exception(
+                    "Unable to create one or more projects: %s" % errs)
+        self.es.indices.refresh(index=self.index)
+
+    def delete_all(self):
+        def gen(docs):
+            for doc in docs:
+                d = {}
+                d['_index'] = self.index
+                d['_type'] = self.dbname
+                d['_op_type'] = 'delete'
+                d['_id'] = doc['_id']
+                yield d
+        bulk(self.es,
+             gen(self.get_all(source=False)))
+        self.es.indices.refresh(index=self.index)
+
+    def load(self, projects):
+        self.delete_all()
+        self.create(projects.iteritems())
+
+    def get_all(self, source=True):
+        query = {
+            '_source': source,
+            'query': {
+                'match_all': {}
+            }
+        }
+        return scanner(self.es, query=query, index=self.index,
+                       doc_type=self.dbname)
+
+    def get_by_id(self, id, source=True):
+        try:
+            res = self.es.get(index=self.index,
+                              doc_type=self.dbname,
+                              _source=source,
+                              id=id)
+            return res['_source']
+        except Exception, e:
+            logger.error('Unable to get the doc. %s' % e)
+
+    def exists(self, id):
+        return self.es.exists(
+            index=self.index, doc_type=self.dbname, id=id)
+
+    def get_by_attr_match(self, attribute, value, source=True):
+        params = {'index': self.index, 'doc_type': self.dbname}
+
+        body = {
+            "query": {
+                'bool': {
+                    'must': {'term': {attribute: value}},
+                }
+            }
+        }
+        params['body'] = body
+        params['_source'] = source
+        res = self.es.search(**params)
+        took = res['took']
+        hits = res['hits']['total']
+        docs = [r['_source'] for r in res['hits']['hits']]
+        return took, hits, docs
+
+    def get_by_nested_attr_match(
+            self, attribute, values, source=True,
+            inner_source=True, inner_hits_max=10000):
+        if not isinstance(values, list):
+            values = (values,)
+        params = {'index': self.index, 'doc_type': self.dbname}
+        body = {
+            "query": {
+                "bool": {
+                    "must": {
+                        "nested": {
+                            "path": "refs",
+                            "inner_hits": {
+                                "_source": inner_source,
+                                "size": inner_hits_max,
+                            },
+                            "query": {
+                                "bool": {
+                                    "should": [
+                                        {"term":
+                                            {"refs.%s" % attribute: value}}
+                                        for value in values
+                                    ]
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        params['body'] = body
+        params['_source'] = source
+        res = self.es.search(**params)
+        inner_hits = [r['inner_hits'] for r in res['hits']['hits']]
+        took = res['took']
+        hits = res['hits']['total']
+        docs = [r['_source'] for r in res['hits']['hits']]
+        return took, hits, docs, inner_hits
+
+
 class Projects(YAMLDefinition):
     """ This class manages definition of projects
     """
-    def __init__(self, db_path=None, db_default_file=None, db_cache_path=None):
-        YAMLDefinition.__init__(self, db_path, db_default_file, db_cache_path)
-        self.enriched = False
-        self.gitweb_lookup = {}
-        self.flatten = {}
-        self.ref2projects_lookup = {}
-        db_path = db_path or conf.get('db_path')
-        db_cache_path = db_cache_path or conf.get('db_cache_path') or db_path
-        self.cached_projects_enriched_path = os.path.join(
-            db_cache_path, 'projects-enriched.cache')
-        self.cached_projects_flatten_path = os.path.join(
-            db_cache_path, 'projects-flatten.cache')
+    def __init__(self, db_path=None, db_default_file=None, db_cache_path=None,
+                 con=None, dump_yaml_in_index=None):
+        self.eprojects = EProjects(
+            connector=(con or index.Connector()))
+        if dump_yaml_in_index:
+            db_path = db_path or conf.get('db_path')
+            YAMLDefinition.__init__(
+                self, db_path, db_default_file, db_cache_path)
+            issues = self.validate()
+            if issues:
+                raise RuntimeError(issues)
+            self._enrich_projects()
+            self.eprojects.load(self._flatten_projects())
 
     def _merge(self):
         """ Merge self.data and inherites from default_data
         """
-        # TODO(fbo): This could even be put in cache
         merged_templates = {}
         merged_projects = {}
         for d in self.data:
@@ -229,34 +405,7 @@ class Projects(YAMLDefinition):
         self.templates.update(merged_templates)
         self.projects.update(merged_projects)
 
-    def _save_to_cache(self, path, hash, data):
-        ddata = (hash, data)
-        if not os.path.isdir(os.path.dirname(path)):
-            os.makedirs(os.path.dirname(path))
-        cPickle.dump(ddata, open(path, 'w'), cPickle.HIGHEST_PROTOCOL)
-
-    def _read_from_cache(self, path, hash):
-        try:
-            registered_hash, data = cPickle.load(open(path))
-        except Exception as err:
-            logger.debug('Unable to read cache %s: %s' % (path, err))
-            return None
-        if hash == registered_hash:
-            return data
-        return None
-
     def _enrich_projects(self):
-        cached_data = self._read_from_cache(
-            self.cached_projects_enriched_path, self.hashes_str)
-        if cached_data is not None:
-            self.projects = cached_data[0]
-            self.gitweb_lookup = cached_data[1]
-            self.enriched = True
-            logger.debug('Read projects enriched from cache %s' % (
-                self.cached_projects_enriched_path))
-        if self.enriched:
-            return
-        # First resolve templates references
         for pid, detail in self.projects.items():
             if 'meta-ref' not in detail:
                 detail['meta-ref'] = False
@@ -303,16 +452,30 @@ class Projects(YAMLDefinition):
                 # Transform date to epoch
                 for release in repo['releases']:
                     release['date'] = date2epoch(release['date'])
-                # Fill the lookup table for gitweb links
-                if 'gitweb' in repo:
-                    su = '%s:%s' % (repo['uri'], rid)
-                    self.gitweb_lookup[su] = repo['gitweb']
-        data = (self.projects, self.gitweb_lookup)
-        self._save_to_cache(
-            self.cached_projects_enriched_path, self.hashes_str, data)
-        logger.debug('Saved projects enriched cache in %s' % (
-            self.cached_projects_enriched_path))
-        self.enriched = True
+
+    def _flatten_projects(self):
+        flatten = {}
+        for pid, detail in self.projects.items():
+            flatten[pid] = {
+                'meta-ref': detail.get('meta-ref'),
+                'repos': [],
+                'description': detail.get('description'),
+                'logo': detail.get('logo'),
+                'bots-group': detail.get('bots-group'),
+            }
+            for rid, repo in detail['repos'].items():
+                for branch in repo['branches']:
+                    r = {}
+                    r.update(copy.deepcopy(repo))
+                    r['name'] = rid
+                    r['aname'] = rid
+                    r['branch'] = branch
+                    del r['branches']
+                    r['fullrid'] = "%s:%s:%s" % (
+                        r['uri'], r['name'], r['branch'])
+                    r['shortrid'] = "%s:%s" % (r['uri'], r['name'])
+                    flatten[pid]['repos'].append(r)
+        return flatten
 
     def _validate_templates(self):
         """ Validate self.data consistencies for templates
@@ -363,71 +526,56 @@ class Projects(YAMLDefinition):
         validation_issues.extend(issues)
         return validation_issues
 
-    def get_projects_raw(self):
-        if not self.enriched:
-            self._enrich_projects()
-        return self.projects
+    def get_projects(self, source=True):
+        if isinstance(source, list) and 'name' not in source:
+            source.append('name')
+        projects = {}
+        for project in list(self.eprojects.get_all(source)):
+            projects[project['_source']['name']] = project['_source']
+        return projects
 
-    def get_projects(self):
-        if self.flatten:
-            return self.flatten
-        cached_data = self._read_from_cache(
-            self.cached_projects_flatten_path, self.hashes_str)
-        if cached_data is not None:
-            self.flatten = cached_data
-            logger.debug('Read projects flatten from cache %s' % (
-                self.cached_projects_flatten_path))
-            return self.flatten
-        if not self.enriched:
-            self._enrich_projects()
-        # This transforms repos into refs by listing
-        # their branches. A project is now
-        # a list of refs
-        for pid, detail in self.projects.items():
-            self.flatten[pid] = {
-                'meta-ref': detail.get('meta-ref'),
-                'repos': [],
-                'description': detail.get('description'),
-                'logo': detail.get('logo'),
-                'bots-group': detail.get('bots-group'),
-            }
-            for rid, repo in detail['repos'].items():
-                for branch in repo['branches']:
-                    r = {}
-                    r.update(copy.deepcopy(repo))
-                    r['name'] = rid
-                    r['branch'] = branch
-                    del r['branches']
-                    self.flatten[pid]['repos'].append(r)
-        self._save_to_cache(
-            self.cached_projects_flatten_path, self.hashes_str, self.flatten)
-        logger.debug('Saved projects flatten cache in %s' % (
-            self.cached_projects_flatten_path))
-        return self.flatten
+    def get(self, pid, source=True):
+        return self.eprojects.get_by_id(pid, source)
 
+    def exists(self, pid):
+        return self.eprojects.exists(pid)
+
+    # TODO(fbo): Refactor to match refs with get_by_nested_attr_match
     def get_tags(self):
-        projects = self.get_projects()
-        tags = {}
-        for _, details in projects.items():
-            for ref in details['repos']:
+        projects = self.get_projects(source=['refs'])
+        tags = set()
+        for project in projects.values():
+            for ref in project['refs']:
                 for tag in ref.get('tags', []):
-                    tags.setdefault(tag, {'repos': []})
-                    tags[tag]['repos'].append(ref)
-        return tags
+                    tags.add(tag)
+        return list(tags)
 
-    def get_ref2projects_lookup(self):
-        if self.ref2projects_lookup:
-            return self.ref2projects_lookup
-        projects = self.get_projects()
-        # Fill ref2project lookup
-        for pname, details in projects.items():
-            for r in details['repos']:
-                full_rid = "%s:%s:%s" % (r['uri'],
-                                         r['name'],
-                                         r['branch'])
-                self.ref2projects_lookup.setdefault(
-                    full_rid, set()).update([pname])
-        return self.ref2projects_lookup
+    def get_gitweb_link(self, fullrid):
+        source = 'name'
+        inner_source = 'gitweb'
+        ret = self.eprojects.get_by_nested_attr_match(
+            'fullrid', fullrid, source, inner_source, 1)
+        # Get the first inner hit / let's see later if that cause limitations
+        if not ret[3]:
+            return ''
+        ref = ret[3][0]['refs']['hits']['hits'][0]['_source']
+        return ref.get('gitweb', '')
 
-    def get_gitweb_link(self, simple_uri):
-        return self.gitweb_lookup.get(simple_uri, "")
+    def get_projects_from_references(self, fullrids):
+        source = 'name'
+        inner_source = 'name'
+        ret = self.eprojects.get_by_nested_attr_match(
+            'fullrid', fullrids, source, inner_source)
+        projects = set([p['name'] for p in ret[2]])
+        return list(projects)
+
+    def get_references_from_tags(self, tags):
+        source = 'name'
+        inner_source = ['fullrid', 'paths', 'name']
+        ret = self.eprojects.get_by_nested_attr_match(
+            'tags', tags, source, inner_source)
+        refs = []
+        for hit in ret[3]:
+            refs.extend([r['_source'] for r
+                         in hit['refs']['hits']['hits']])
+        return refs
