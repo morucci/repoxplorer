20df702d1f287aa8d8678f154f33cd54f503206d
Matthieu Huin <mhuin@redhat.com>
2019-05-22 16:03:16 +0200

    [WIP] Add OpenID Connect support
    Allow operator to configure an authentication engine relying on OpenID
    Connect. Currently only the backend part is written: the 'users' API
    endpoint can validate and read a JWT issued by a known OpenID provider.
    
    TG-2606
    
    Change-Id: Ibf89930cfc3988da8eb5e181bed2bb3df74a8fd0

---
+++ repoxplorer/tests/fixtures/config.py
@@ -0,0 +1,32 @@
+import tempfile
+from repoxplorer.controllers.renderers import CSVRenderer
+
+# Server Specific Configurations
+server = {
+    'port': '8080',
+    'host': '0.0.0.0'
+}
+
+# Pecan Application Configurations
+app = {
+    'root': 'repoxplorer.controllers.root.RootController',
+    'modules': ['repoxplorer'],
+    'custom_renderers': {'csv': CSVRenderer},
+    'static_root': '%(confdir)s/../../public',
+    'debug': True,
+    'errors': {
+        '404': '/error/404',
+        '__force_dict__': True
+    }
+}
+
+projects_file_path = None
+git_store = None
+db_path = tempfile.mkdtemp()
+db_default_file = None
+xorkey = None
+elasticsearch_host = 'localhost'
+elasticsearch_port = 9200
+elasticsearch_index = 'repoxplorertest'
+
+admin_token = '12345'
